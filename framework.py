1d7dcbd633d76a55edb1f28fe2b09f175126f44c
JayceFR <jaycejefferson.vicious@gmail.com>
2022-10-21 16:10:38 +0100

    added_aim

--- framework.py
+++ framework.py
@@ -48,3 +48,15 @@ class Map():
                     window.blit(self.tile1, (x * 16 - scroll[0], y * 16 - scroll[1]) )
                 x += 1
             y += 1
+#Bullets
+class Bullet():
+    def __init__(self, bullet_pos) -> None:
+        self.bullet_pos = bullet_pos
+        self.rect = pygame.rect.Rect(bullet_pos[0], bullet_pos[1], 3,3)
+    
+    def draw(self, window):
+        pygame.draw.circle(window, (255,0,0), self.bullet_pos, 2)
+    
+    def move_bullet(self):
+        self.rect.x += 5
+        self.bullet_pos[0] += 5
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame 
 import framework as f
-
+pygame.init()
+#Display settings 
 screen_w = 1000
 screen_h = 600
 window = pygame.display.set_mode((screen_w,screen_h))
 display = pygame.Surface((screen_w//2, screen_h//2))
 pygame.display.set_caption("YUIOP")
+
+#Game Attributes
 run = True
+clock = pygame.time.Clock()
 player = f.Player([30,30])
 tile1 = pygame.image.load("Assets/tile1.png")
 map = f.Map("Assets/map.txt",tile1 )
 true_scroll = [0,0]
 scroll = [0,0]
+
+#Mouse Settings
+pygame.mouse.set_visible(False)
+
+#Main Game Loop
 while run:
+    clock.tick(60)
     display.fill((0,0,0))
+    #Mouse Settings 
+    mpos = pygame.mouse.get_pos()
     map.blit_map(display, scroll)
     #Calculating Scroll
     true_scroll[0] += (player.get_rect().x - true_scroll[0] - 262) / 20
@@ -24,6 +36,8 @@ while run:
     #Player Blitting
     player.move()
     player.draw(display, scroll)
+    #Mouse Blitting
+    pygame.draw.circle(display,(200,0,0), (mpos[0]//2, mpos[1]//2), 4)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
